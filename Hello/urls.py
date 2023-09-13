3614fa9695d68afa09d1f4fc1c2231b7298e1598
YogitaJadhav <yogita.more30@gmail.com>
2023-09-13 10:26:34 +0530

    login updated

--- Hello/urls.py
+++ Hello/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", include("website.urls")),
     path("dashboard/", include("dashboard.urls")),
     path("Blog/", include("Blog.urls")),
     path("admin/", admin.site.urls),
+    path("login/",auth_views.LoginView.as_view(),name="login")
 ]
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+from . import views
 
 from . import views
 app_name= "dashboard"
 urlpatterns = [
-    path("", views.DashboardView.as_view(), name="msg"),
-    path("Blog/", views.BlogView.as_view(), name="blog"),
-    path("add_blog/", views.add_blog, name="add_blog"),
-     path("<int:pk>/delete_blog/", views.delete_blog, name="delete_blog"),
+    path("", login_required(views.DashboardView.as_view()), name="msg"),
+    path("Blog/", login_required(views.BlogView.as_view()), name="blog"),
+    path("add_blog/", login_required(views.add_blog), name="add_blog"),
+    path("<int:pk>/delete_blog/", login_required(views.delete_blog), name="delete_blog"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout")
+     
 ]
